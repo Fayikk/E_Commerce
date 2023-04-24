0a5bfd450374b01a3d3d968a04c5e5f6e3cd654a
Fayik <veznedaroglufayik2@gmail.com>
2023-04-24 21:22:16 +0300

    Add project files.

---
+++ E_CommerceForUdemy_Business/Mapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using E_CommerceForUdemy_DataAccess;
+using ECommerce_ForUdemy_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceForUdemy_Business.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
---
+++ E_CommerceForUdemy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce_ForUdemy_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceForUdemy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<CategoryDTO> Create(CategoryDTO objDTO);
+        public Task<CategoryDTO> Update(CategoryDTO objDTO);
+        public Task<int> Delete(int id);
+        public Task<CategoryDTO> Get(int id);
+        public Task<IEnumerable<CategoryDTO>> GetAll();
+    }
+}
